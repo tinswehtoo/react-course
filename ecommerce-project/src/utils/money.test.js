ad56d3a46de8f03fdb2b1ff9a22ecc7d8ae9b55c
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2026-01-02 21:00:04 +0630

    changes formatMoney and select a quantity

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -10,4 +10,13 @@ describe("formatMoney", () => {
     expect(formatMoney(100)).toBe("$1.00");
     expect(formatMoney(1090)).toBe("$10.90");
   });
+
+  it("work with the number 0", () => {
+    expect(formatMoney(0)).toBe("$0.00");
+  });
+
+  it("work with negative number", () => {
+    expect(formatMoney(-999)).toBe("-$9.99");
+    expect(formatMoney(-100)).toBe("-$1.00");
+  });
 });
