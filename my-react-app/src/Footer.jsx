7bde2e10442fbb3b22bb0273910a88deb7002590
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2026-01-12 20:49:19 +0630

    Create my-react-app and Header,footer and food

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Your Website name</p>
+    </footer>
+  );
+}
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>My website</h1>
+      <nav>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Service</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </nav>
+    </header>
+  );
+}
