f2fdf83c1996f3b17a29da42b9b6ef2e37c9a888
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2026-01-13 19:02:09 +0630

    Create a component card

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Food from "./Food";
+import Card from "./Card";
 
 function App() {
   return (
     <>
+      <Card />
       <Header />
       <Food />
       <Footer />
