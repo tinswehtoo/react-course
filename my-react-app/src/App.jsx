faca535a30e91a7140b68ee73f1d8bb0d7b33673
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2026-01-13 20:14:36 +0630

    Create a component Student and about Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,12 +3,18 @@ import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
 import Button from "./Button/Button";
+import Student from "./Student/Student";
 
 function App() {
   return (
     <>
       <Card />
       <Button />
+      <Student name="Spongebob" age={32} isStudent={true} />
+      <Student name="Larry" age={40} isStudent={false} />
+      <Student name="Patrick" age={39} isStudent={false} />
+      <Student name="Justin" age={60} isStudent={true} />
+
       <Header />
       <Food />
       <Footer />
