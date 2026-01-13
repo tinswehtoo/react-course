99f0b5682fa7952686da725607a5e07408c6abc9
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2026-01-13 20:15:17 +0630

    Create a component Student and about Props

---
+++ my-react-app/src/Student/Student.jsx
@@ -0,0 +1,12 @@
+import "./Student.css";
+import PropTypes from "prop-types";
+
+export default function Student(props) {
+  return (
+    <div className="student">
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+    </div>
+  );
+}
