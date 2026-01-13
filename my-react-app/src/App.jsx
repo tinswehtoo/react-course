49c1d1d06746afb4860190ab609c4b3457a5fb29
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2026-01-13 19:18:45 +0630

    Create component for button and button.module.css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import Button from "./Button/Button";
 
 function App() {
   return (
     <>
       <Card />
+      <Button />
       <Header />
       <Food />
       <Footer />
